51127f51cc1ee0e5c28ed8a50ecd2f9e218e7778
ShakeyAl <smith.alexander.e@gmail.com>
2015-01-25 01:24:04 -0400

    Update Menu.java

--- src/interfacePackage/Menu.java
+++ src/interfacePackage/Menu.java
@@ -1,13 +1,25 @@
 package interfacePackage;
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Menu {
-	
+	int selection = 0;
 	public Menu(){}
 	
-	public int MainMenu(){
+	public int MainMenu{
+		/************************/
+		System.out.println("Welcome to Al's RPG");
+		System.out.println();
+		System.out.println("Please select an option from below list:");
+		System.out.println("1. Create new character");
+		System.out.println("2. Load character");
+		System.out.println();
+		System.out.print("0. Exit Game");
+		/*************************/
 		
-		return 1;
+		Scanner sc = new Scanner(System.in);
+		selection = sc.nextInt();
+	
+		return selection;
 	}
 	
 }
