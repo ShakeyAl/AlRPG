04dea8ee7be8eeb04121320e97e0e20b26c31f3e
ShakeyAl <smith.alexander.e@gmail.com>
2015-01-25 00:53:50 -0400

    Update Game.java added basic menu logic
    Added the menu selection options, but commented it out as I don't have character class created yet.

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,18 +1,29 @@
 package game;
-// import interfacePackage.*;
+import interfacePackage.Menu;
 
 public class Game {
+	int menuOneSelection;
 	public Game(){}
 	
 	public void start(){
+		Boolean gameOver = false;
+		while(gameOver == false){
+			menuOneSelection = Menu.MainMenu();
+			// if(menuOneSelection == 1) create new character
+			// else if(menuOneSelection == 2) Load already created character
+			// else gameOver = true;
+			
+			//take loaded character and contine to playing game
+			
+			gameOver = true; //  this is for testing just so i'm not stuck in an infinite loop
+			
+		}
+	}
+	/*
+	
+	public void playGame( database value ){
 		
-		// while ( int i != 0 )
-		// display Main Menu
-		// get a selection from it
-		// if not create or load end loop, exit game
-		
-		// if 1  create character ( launch character creation)
-		// if 2  load character ( lauch character selection)
 	}
 	
+	*/
 }
